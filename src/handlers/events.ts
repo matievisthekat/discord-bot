6c92521c7916f066a5ef0eb886c13585d63f4d56
matievisthekat <matievisthekat@gmail.com>
2022-11-13 19:39:22 +0200

    add events handler

---
+++ src/handlers/events.ts
@@ -0,0 +1,24 @@
+import { Events } from "discord.js";
+import findFiles from "../util/findFiles";
+
+export interface Event {
+  name: Events,
+  once: boolean,
+  execute(...args: any): Promise<void> | void
+}
+
+export async function findEvents(baseDir: string) {
+  const res: Event[] = [];
+  const files = await findFiles(baseDir, 'js');
+
+  for (const file of files) {
+    const {default: mod} = await import(file);
+    if ('name' in mod && 'execute' in mod) {
+      if (Object.keys(Events).some((e) => e === mod.name) && typeof mod.execute === 'function') {
+        res.push(mod);
+      }
+    }
+  }
+
+  return res;
+} 
