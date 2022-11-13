5a50526af197940d85f59d77350e12883340156d
matievisthekat <matievisthekat@gmail.com>
2022-11-13 19:46:04 +0200

    add first event

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,10 +1,10 @@
-import { Client, Events } from "discord.js";
-import { info } from "../logger";
+import {Client, Events} from 'discord.js';
+import {info} from '../logger';
 
 export default {
-  name: Events.ClientReady,
-  once: true,
-  execute(client: Client) {
-    info(`Logged in as ${client.user.tag}`)
-  }
-}
+	name: Events.ClientReady,
+	once: true,
+	execute(client: Client<true>) {
+		info(`Logged in as ${client.user.tag}`);
+	}
+};
