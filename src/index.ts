aa1970dba666c5a427181413bd473978317ea447
matievisthekat <matievisthekat@gmail.com>
2022-11-13 19:45:59 +0200

    use command and event handlers

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,37 @@
 import {config} from 'dotenv';
 config();
 
-import {Client, ClientOptions, Collection, Events, GatewayIntentBits, ModalSubmitFields} from 'discord.js';
-import { Command, findCommands } from './commands';
-import { info } from './logger';
+import {Client, ClientEvents, ClientOptions, Collection, Events, GatewayIntentBits} from 'discord.js';
+import {Command, findCommands} from './handlers/commands';
+import {findEvents, Event} from './handlers/events';
+import {join} from 'path';
+import {info} from './logger';
 
 class Bot extends Client {
-  commands: Collection<string, Command> = new Collection();
+	commands: Collection<string, Command> = new Collection();
+	events: Collection<keyof ClientEvents, Event> = new Collection();
 
-  constructor(opts: ClientOptions) {
-    super(opts);
-  }
+	constructor(opts: ClientOptions) {
+		super(opts);
+	}
 
-  async populateCommands() {
-    const files = await findCommands('/');
-    files.forEach((f) => this.commands.set(f.slash.name, f));
-    return this.commands;
-  }
+	async populateCommands() {
+		const cmds = await findCommands(join(__dirname, 'commands'));
+		cmds.forEach((c) => this.commands.set(c.slash.name, c));
+	}
+
+	async populateEvents() {
+		const events = await findEvents(join(__dirname, 'events'));
+		events.forEach((e) => {
+			this.events.set(e.name, e);
+			this[e.once ? 'once' : 'on'](e.name, (...args) => e.execute(...args));
+		});
+	}
 }
 
 const client = new Bot({intents: [GatewayIntentBits.Guilds]});
 
-client.on(Events.ClientReady, (c) => {
-  info(`Logged in as ${c.user.tag}`);
-});
-
 client.populateCommands().then(async () => {
-  await client.login(process.env.TOKEN);
+	await client.populateEvents();
+	await client.login(process.env.TOKEN);
 });
